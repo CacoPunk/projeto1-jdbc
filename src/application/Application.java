c8534e3f807e0b1e2332ee2b50482c88dc1dc1ef
Paulo Ridardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-03-17 11:37:00 -0300

    Seller entitie class

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Application {
 
 	public static void main(String[] args) {
+		SimpleDateFormat sdf  = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "books");
 		System.out.println(obj);
-
+		Seller seller = new Seller(1, "paulo", "pr@pr.com", new Date(0), 2500.00, obj);
+		System.out.println(seller);
 	}
 
 }
