7d03a73fa0e6d6ca43ba21784f123660503622ef
Paulo Ridardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-03-17 14:41:28 -0300

    findBuId implementation

--- src/application/Application.java
+++ src/application/Application.java
@@ -11,13 +11,10 @@ import model.entities.Seller;
 public class Application {
 
 	public static void main(String[] args) {
-		SimpleDateFormat sdf  = new SimpleDateFormat("dd/MM/yyyy");
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
-		Seller seller = new Seller(1, "paulo", "pr@pr.com", new Date(0), 2500.00, obj);
-		System.out.println(seller);
-		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
